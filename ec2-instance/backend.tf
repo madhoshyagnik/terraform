2f14f464dd6a3d4eb2441473ca6459ac95854f17
madhoshyagnik <madhosh1yagnik@gmail.com>
2024-08-30 18:49:34 +0530

    s3 for statefile, dynamodb for terraform lock

--- ec2-instance/backend.tf
+++ ec2-instance/backend.tf
@@ -1,7 +1,10 @@
+# create bucket and dynamodb table first before using backend to avoid issues
+
 terraform {
   backend "s3" {
     bucket = "terraform-state-file-123129038290"
     key    = "statefile/terraform.tfstate"
     region = "ap-south-1"
+    # dynamodb_table = "terraform_lock"
   }
 }
--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -7,3 +7,18 @@ resource "aws_instance" "terraform_instance" {
     instance_type = "t2.micro"
     key_name = "debian"
 }
+
+# create s3 for statefile earlier
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  
+}
