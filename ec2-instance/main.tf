dc9d1aa5fc97c37ff5c8fcc847949aedd578d9ea
madhoshyagnik <madhosh1yagnik@gmail.com>
2024-08-30 16:35:31 +0530

    adding files

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -5,12 +5,28 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+variable "ami_id" {
+    description = "some description"
+    type = string
+    default = "ami-02b49a24cfb95941c"
+}
+
+variable "key_name" {
+    type = string
+    default = "debian"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
 
 resource "aws_instance" "terraform_instance" {
-    ami = "ami-02b49a24cfb95941c"
-    key_name = "debian"
-    instance_type = "t2.micro"
-    
+    ami = var.ami_id
+    key_name = var.key_name
+    instance_type = var.instance_type
+}
 
-    
+output "public_ip" {
+    value = aws_instance.terraform_instance.public_dns
 }
