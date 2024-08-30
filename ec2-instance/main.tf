68aca1ac3710d71ea324d0cf53604c65abf5c584
madhoshyagnik <madhosh1yagnik@gmail.com>
2024-08-30 18:15:59 +0530

    addding s3 bucket for statefile

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,25 +1,9 @@
 provider "aws" {
     region = "ap-south-1"
-  
-}
-
-variable "ami_id" {
-    description = "value of the ami"
-  
-}
-
-variable "instance_type" {
-    description = "value for instance type"
-  
-}
-
-variable "key_name" {
-    description = "key you want to use to launch the instance"
-  
 }
 
 resource "aws_instance" "terraform_instance" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
+    ami = "ami-0522ab6e1ddcc7055"
+    instance_type = "t2.micro"
+    key_name = "debian"
 }
