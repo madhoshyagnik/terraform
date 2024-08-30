5798a0082d011aa69f0b8547a90389acd1eff0fb
madhoshyagnik <madhosh1yagnik@gmail.com>
2024-08-30 16:58:29 +0530

    adding files

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,32 +1,25 @@
-#ubuntu ami
-# ami-02b49a24cfb95941c
-
 provider "aws" {
-  region = "ap-south-1"
+    region = "ap-south-1"
+  
 }
 
 variable "ami_id" {
-    description = "some description"
-    type = string
-    default = "ami-02b49a24cfb95941c"
-}
-
-variable "key_name" {
-    type = string
-    default = "debian"
+    description = "value of the ami"
+  
 }
 
 variable "instance_type" {
-    type = string
-    default = "t2.micro"
+    description = "value for instance type"
+  
+}
+
+variable "key_name" {
+    description = "key you want to use to launch the instance"
+  
 }
 
 resource "aws_instance" "terraform_instance" {
     ami = var.ami_id
-    key_name = var.key_name
     instance_type = var.instance_type
-}
-
-output "public_ip" {
-    value = aws_instance.terraform_instance.public_dns
+    key_name = var.key_name
 }
