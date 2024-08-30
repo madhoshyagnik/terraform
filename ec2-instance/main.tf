2931e22b3409baebca5f665ffb35051c47754a10
madhoshyagnik <madhosh1yagnik@gmail.com>
2024-08-30 17:28:20 +0530

    added terraform modules

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
     region = "ap-south-1"
-  
 }
 
-resource "aws_instance" "terraform_instance" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
+module "ec2_instance" {
+    source = "./module/ec2_instance"
+    ami_id = "ami-0522ab6e1ddcc7055"
+    instance_type = "t2.micro"
+    key_name = "debian"
+}
+
+module "aws_s3_bucket" {
+    source = "./module/s3"
+    bucket_name = "terraform-test-bucket-123123123123" 
 }
